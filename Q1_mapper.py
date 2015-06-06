b026e033159b0b6f228ac46795f048ad1f8fff6b
Rich <rb4179@att.com>
2015-06-06 08:42:59 -0500

    Q1 Mapper Code
    This is the Python mapper code for question 1 in the udacity hadoop/mapreduce class

---
+++ Q1_mapper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# We want elements 2 (store name) and 4 (cost)
+# We need to write them out to standard output, separated by a tab
+
+import sys
+
+for line in sys.stdin:
+    data = line.strip().split("\t")
+    if len(data) == 6:
+        date, time, store, item, cost, payment = data
+        print "{0}\t{1}".format(item, cost)
+        #print "{0}\t{1}\t{2}".format(store, item, cost)
+        #print store item cost
+# added item for breakdown by product cat
